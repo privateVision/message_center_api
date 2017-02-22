213f5d9de5a9eeb39207f450c83b40961b9a50cc
lixingxing <lixingxing@anfan.com>
2017-02-22 15:20:29 +0800

    update

---
+++ appapi/app/Http/Controllers/Api/PhoneController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Api\BaseController;
+use Illuminate\Support\Facades\Request;
+
+/*
+ * 手机短信处理相关的操作
+ * */
+class PhoneController extends BaseController{
+    //发送短信
+    public function sendCodeAction(Request $request){
+
+    }
+
+}
