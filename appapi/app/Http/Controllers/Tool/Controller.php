aa9015e762102f58c6836ac177b6b6627020d2d6
lixingxing <lixingxing@anfan.com>
2017-02-23 09:56:26 +0800

    update

--- appapi/app/Http/Controllers/Tool/Controller.php
+++ appapi/app/Http/Controllers/Tool/Controller.php
@@ -30,7 +30,6 @@ class Controller extends \App\Controller
         } catch (ToolException $e) {
             return array('code' => $e->getCode(), 'msg' => $e->getMessage(), 'data' => null);
         } catch(\Exception $e) {
-            // todo: 打印这么详细的消息到客户端是不安全的，方便调试
             return array('code' => ToolException::Error, 'msg' => sprintf('%s in %s(%d)', $e->getMessage(), $e->getFile(), $e->getLine()), 'data' => null);
         }
     }
