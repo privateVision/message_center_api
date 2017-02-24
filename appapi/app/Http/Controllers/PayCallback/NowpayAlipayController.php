996e2fbde5587c5ef446e3ff27ace8bea0460c61
lixingxing <lixingxing@anfan.com>
2017-02-24 10:41:13 +0800

    支付回调框架结构完成,可以开始回调逻辑了

---
+++ appapi/app/Http/Controllers/PayCallback/NowpayAlipayController.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Controller\External;
+
+use App\Exceptions\PayCallbackException;
+use Illuminate\Http\Request;
+use App\Model\Orders;
+
+class NowpayAlipayController extends \App\Controller
+{
+
+    protected function getData(Request $request) {
+
+    }
+
+    protected function getOrderNo($data) {
+
+    }
+
+    protected function getOuterOrderNo($data, Orders $order) {
+
+    }
+
+    protected function verifySign($data, Orders $order) {
+
+    }
+
+    protected function handler($data, Orders  $order){
+
+    }
+
+    protected function onComplete($data, Orders $order, $isSuccess, $code) {
+        
+    }
+}
