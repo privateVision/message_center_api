0773d77b0664ea437d4bc22e1b62c6fc850b7aa0
lixingxing <lixingxing@anfan.com>
2017-03-04 13:43:24 +0800

    队列处理脚本的启动和重启

--- appapi/listen-queue.sh
+++ appapi/listen-queue.sh
@@ -2,7 +2,8 @@
 # --timeout= 任务处理超时时间，单位：秒
 # --tries= 任务失败最大重试次数
 # --daemon 守护进程（后台运行）
-/usr/bin/php artisan queue:work --daemon --sleep=1 --timeout=45 --tries=86400
+/usr/bin/php artisan queue:restart
+/usr/bin/php artisan queue:work --sleep=1 --timeout=45 --tries=86400
 # 重启进程
 #/usr/bin/php artisan queue:restart
 # 查看所有失败的任务
