f0bed8a2a00a46284356e0b30a7d7a897ec02d4c
lixingxing <lixingxing@anfan.com>
2017-03-07 09:31:12 +0800

    update

--- appapi/app/Jobs/Log.php
+++ appapi/app/Jobs/Log.php
@@ -22,18 +22,8 @@ class Log extends Job
     public function handle()
     {
         if($this->content['level'] === 'ERROR') {
-            send_mail('SDK接口调用错误', env('ALARM_MAILS'), json_encode($this->content, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+            send_mail('SDK接口调用错误', explode('|', env('ALARM_MAILS')), json_encode($this->content, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
         }
-/*
-        // --------------
-        $rk = new \RdKafka\Producer();
-        $rk->setLogLevel(LOG_DEBUG);
-        $rk->addBrokers("127.0.0.1");
-*/
-        $kafka_producer = app('kafka_producer');
-        $topic = $kafka_producer->newTopic("test");
-        $topic->produce(RD_KAFKA_PARTITION_UA, 0, 'test message');
-        // ---------------
 
         AppApiLog::insert($this->content);
     }
