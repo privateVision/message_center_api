ab55caab49ea96c40a71d9625735f15fbdc463f3
yangchujie <ycj@anfan.com>
2017-03-16 19:19:43 +0800

    [pub]修改公告获取

--- message_service_api/Controller/NoticeController.py
+++ message_service_api/Controller/NoticeController.py
@@ -118,11 +118,11 @@ def v4_sdk_get_notice_list():
     if is_sign_true:
         ucid = get_ucid_by_access_token(request.form['_token'])
         if ucid:
-            page = request.form['page'] if request.form.has_key('page') and request.form['page'] else 1
-            count = request.form['count'] if request.form.has_key('count') and request.form['count'] else 10
-            start_index = (int(page) - 1) * int(count)
-            end_index = start_index + int(count)
-            service_logger.info("用户：%s 获取公告列表，数据从%s到%s" % (ucid, start_index, end_index))
+            # page = request.form['page'] if request.form.has_key('page') and request.form['page'] else 1
+            # count = request.form['count'] if request.form.has_key('count') and request.form['count'] else 10
+            # start_index = (int(page) - 1) * int(count)
+            # end_index = start_index + int(count)
+            # service_logger.info("用户：%s 获取公告列表，数据从%s到%s" % (ucid, start_index, end_index))
             # 查询用户相关的公告列表
             current_timestamp = get_current_timestamp()
             # message_list_total_count = UserMessage.objects(
@@ -139,45 +139,58 @@ def v4_sdk_get_notice_list():
                 # & Q(is_read=0)
                 & Q(start_time__lte=current_timestamp)
                 & Q(end_time__gte=current_timestamp)
-                & Q(ucid=ucid)).order_by('sortby')[start_index:end_index]
-            data_list = []
+                & Q(ucid=ucid)).order_by('sortby, -create_time')[:1]
+            # data_list = []
+            message_resp = {
+                'type': 0,
+                'title': '',
+                'content': '',
+                'url': '',
+                'show_times': 0
+            }
             for message in message_list:
                 message_info = get_notice_message_detail_info(message['mysql_id'])
-                message_resp = {
-                    "meta_info": {},
-                    "head": {},
-                    "body": {}
-                }
-                message_resp['meta_info']['app'] = message_info['app']
-                message_resp['meta_info']['rtype'] = message_info['rtype']
-                message_resp['meta_info']['vip'] = message_info['vip']
-                message_resp['head']['title'] = message_info['title']
-                message_resp['head']['type'] = message_info['type']
-                message_resp['head']['mysql_id'] = message_info['mysql_id']
-                message_resp['head']['atype'] = message_info['atype']
-                message_resp['body']['content'] = message_info['content']
-                message_resp['body']['button_content'] = message_info['button_content']
-                message_resp['body']['button_type'] = message_info['button_type']
-                message_resp['body']['button_url'] = message_info['button_url']
-                message_resp['body']['end_time'] = message_info['end_time']
-                message_resp['body']['enter_status'] = message_info['enter_status']
-                message_resp['body']['img'] = message_info['img']
-                message_resp['body']['open_type'] = message_info['open_type']
-                message_resp['body']['start_time'] = message_info['start_time']
-                message_resp['body']['end_time'] = message_info['end_time']
-                message_resp['body']['show_times'] = message_info['show_times']
-                message_resp['body']['url'] = message_info['url']
-                message_resp['body']['url_type'] = message_info['url_type']
-                data_list.append(message_resp)
-            message_list_total_count = len(data_list)
+                message_resp['type'] = message_info['type']
+                message_resp['title'] = message_info['title']
+                message_resp['content'] = message_info['content']
+                message_resp['url'] = message_info['url']
+                message_resp['show_times'] = message_info['show_times']
+                # message_resp = {
+                #     "meta_info": {},
+                #     "head": {},
+                #     "body": {}
+                # }
+                # message_resp['meta_info']['app'] = message_info['app']
+                # message_resp['meta_info']['rtype'] = message_info['rtype']
+                # message_resp['meta_info']['vip'] = message_info['vip']
+                # message_resp['head']['title'] = message_info['title']
+                # message_resp['head']['type'] = message_info['type']
+                # message_resp['head']['mysql_id'] = message_info['mysql_id']
+                # message_resp['head']['atype'] = message_info['atype']
+                # message_resp['body']['content'] = message_info['content']
+                # message_resp['body']['button_content'] = message_info['button_content']
+                # message_resp['body']['button_type'] = message_info['button_type']
+                # message_resp['body']['button_url'] = message_info['button_url']
+                # message_resp['body']['end_time'] = message_info['end_time']
+                # message_resp['body']['enter_status'] = message_info['enter_status']
+                # message_resp['body']['img'] = message_info['img']
+                # message_resp['body']['open_type'] = message_info['open_type']
+                # message_resp['body']['start_time'] = message_info['start_time']
+                # message_resp['body']['end_time'] = message_info['end_time']
+                # message_resp['body']['show_times'] = message_info['show_times']
+                # message_resp['body']['url'] = message_info['url']
+                # message_resp['body']['url_type'] = message_info['url_type']
+                # data_list.append(message_resp)
+            message_list_total_count = len(message_list)
             # 用户没有公告，重设redis标记，避免再次获取
             if message_list_total_count == 0:
+                message_resp = []
                 RedisHandle.clear_user_data_mark_in_redis(ucid, 'notice')
-            data = {
-                "total_count": message_list_total_count,
-                "data": data_list
-            }
-            return response_data(http_code=200, data=data)
+            # data = {
+            #     "total_count": message_list_total_count,
+            #     "data": message_resp
+            # }
+            return response_data(http_code=200, data=message_resp)
         else:
             log_exception(request, "根据token: %s 获取ucid失败" % (request.form['_token'],))
             return response_data(200, 0, '根据token获取ucid失败')
