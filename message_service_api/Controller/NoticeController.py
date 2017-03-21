803d75a268f7b8249edddc03a48b19d88bdfe8dd
yangchujie <ycj@anfan.com>
2017-03-21 17:24:31 +0800

    [pub]修改csm公告修改和公告关闭和打开功能

--- message_service_api/Controller/NoticeController.py
+++ message_service_api/Controller/NoticeController.py
@@ -21,7 +21,7 @@ notice_controller = Blueprint('NoticeController', __name__)
 
 
 # CMS 发送公告
-@notice_controller.route('/msa/v4/notice', methods=['POST'])
+@notice_controller.route('/msa/v4/add_notice', methods=['POST'])
 @cms_api_request_check
 def v4_cms_post_notice():
     form = PostNoticesRequestForm.from_json(request.json)  # JSON 数据转换
@@ -45,10 +45,10 @@ def v4_cms_post_notice():
 
 
 # CMS 更新公告
-@notice_controller.route('/msa/v4/notice', methods=['PUT'])
+@notice_controller.route('/msa/v4/update_notice', methods=['POST'])
 @cms_api_request_check
 def v4_cms_update_post_notice():
-    form = PostNoticesRequestForm(request.form)  # JSON 数据转换
+    form = PostNoticesRequestForm.from_json(request.json)  # JSON 数据转换
     if not form.validate():
         log_exception(request, "更新公告请求校验异常：%s" % (form.errors,))
         return response_data(200, 0, '客户端请求错误')
@@ -65,7 +65,7 @@ def v4_cms_update_post_notice():
 @notice_controller.route('/msa/v4/notice/close', methods=['POST'])
 @cms_api_request_check
 def v4_cms_set_post_notice_closed():
-    notice_id = request.form['id']
+    notice_id = request.json.get('id')
     if notice_id is None or notice_id == '':
         log_exception(request, "客户端请求错误-notice_id为空")
         return response_data(200, 0, '客户端请求错误')
@@ -82,7 +82,7 @@ def v4_cms_set_post_notice_closed():
 @notice_controller.route('/msa/v4/notice/open', methods=['POST'])
 @cms_api_request_check
 def v4_cms_set_post_notice_open():
-    notice_id = request.form['id']
+    notice_id = request.json.get('id')
     if notice_id is None or notice_id == '':
         log_exception(request, "客户端请求错误-notice_id为空")
         return response_data(200, 0, '客户端请求错误')
