281f7e87bd316cbbdb2527276d2e7cd926b5aa33
lixingxing <lixingxing@anfan.com>
2017-03-21 10:30:07 +0800

    [pub]update

--- appapi/app/Model/UserProcedureExtra.php
+++ appapi/app/Model/UserProcedureExtra.php
@@ -3,6 +3,8 @@ namespace App\Model;
 
 class UserProcedureExtra extends Model
 {
+    const Status_Normal = 0; // 正常
+    
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
 
