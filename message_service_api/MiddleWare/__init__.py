68f34ec8ac31d6d942cca1be594aafd04bd90a6e
yangchujie <ycj@anfan.com>
2017-03-31 16:18:43 +0800

    [pub]调试mysql连接参数

--- message_service_api/MiddleWare/__init__.py
+++ message_service_api/MiddleWare/__init__.py
@@ -73,11 +73,11 @@ def create_app():
 
     app.config['SQLALCHEMY_DATABASE_URI'] = app.config.get('SQLALCHEMY_DATABASE_URI')
     mysql_engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], encoding="utf-8", echo=True,
-                                 pool_recycle=28800, pool_size=20)
+                                 pool_recycle=1, pool_size=20)
     mysql_session = sessionmaker(autocommit=False, bind=mysql_engine)
 
     mysql_cms_engine = create_engine(app.config['SQLALCHEMY_CMS_DATABASE_URI'], encoding="utf-8", echo=True,
-                                     pool_recycle=28800, pool_size=20)
+                                     pool_recycle=1, pool_size=20)
     mysql_cms_session = sessionmaker(autocommit=False, bind=mysql_cms_engine)
 
     return app, kafka_producer, mysql_session(), mysql_cms_session()
