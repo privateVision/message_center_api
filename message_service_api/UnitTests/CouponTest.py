b18cd5d7eaf2b718d0175ae61e396a2449204aec
yangchujie <ycj@anfan.com>
2017-03-31 16:55:54 +0800

    [pub]修改单元测试

--- message_service_api/UnitTests/CouponTest.py
+++ message_service_api/UnitTests/CouponTest.py
@@ -5,6 +5,8 @@ import unittest
 import requests
 from mongoengine import Q, connect
 
+from Utils.EncryptUtils import get_cms_md5_sign
+
 """
 卡券功能测试
 """
@@ -14,67 +16,64 @@ class CouponFunctionTest(unittest.TestCase):
     def setUp(self):
         connect('users_message', host='localhost', port=27017)
 
-    # def test_add_coupon(self):
-    #     from MongoModel.MessageModel import UsersMessage
-    #     from MongoModel.UserMessageModel import UserMessage
-    #     from MongoModel.UserReadMessageLogModel import UserReadMessageLog
-    #     UsersMessage.objects(Q(type='coupon')).delete()
-    #     UserMessage.objects(Q(type='coupon')).delete()
-    #     UserReadMessageLog.objects(Q(type='coupon')).delete()
-    #     data = {
-    #         "game": "[{\"apk_id\": 783}]",
-    #         "users_type": "255,3",
-    #         "vip_user": "3,4",
-    #         "specify_user": "123,324",
-    #         "id": "1",
-    #         "title": "测试卡券",
-    #         "stime": "123234533",
-    #         "etime": "123432453",
-    #         "is_first": "1",
-    #         "info": "简单描述",
-    #         "full": "1231223",
-    #         "money": "41223",
-    #         "method": "使用方法",
-    #         "num": "11"
-    #     }
-    #     from Utils.EncryptUtils import get_md5_sign
-    #     sign = get_md5_sign(data)
-    #     r = requests.post("http://localhost:5000/v4/coupon?sign=%s" % (sign,), data=data)
-    #     self.assertEqual(r.status_code, 200)
-    #     print r.text
+    def test_add_coupon(self):
+        data = {
+            "game": [{"apk_id": 783}],
+            "users_type": "255,3",
+            "vip_user": "3",
+            "specify_user": "123,324",
+            "id": 1,
+            "title": "测试卡券",
+            "stime": 123234533,
+            "etime": 123432453,
+            "is_first": 1,
+            "info": "简单描述",
+            "full": 1231223,
+            "money": 41223,
+            "method": "使用方法",
+            "num": 11
+        }
+        headers = {"Content-Type": "application/json"}
+        data_json = json.dumps(data)
+        sign = get_cms_md5_sign(data_json)
+        r = requests.post("http://dev.sdkapi.com/msa/v4/coupon?sign=%s" % (sign,), data=data_json, headers=headers)
+        self.assertEqual(r.status_code, 200)
+        print r.text
 
-    # def test_update_coupon(self):
-    #     data = {
-    #         "game": "[{\"apk_id\": 783}]",
-    #         "users_type": "255,3",
-    #         "vip_user": "3,4",
-    #         "specify_user": "123,324",
-    #         "id": "1",
-    #         "title": "测试卡券(更新)",
-    #         "stime": "123234300",
-    #         "etime": "123432333",
-    #         "is_first": "1",
-    #         "info": "简单描述（更新）",
-    #         "full": "1231223",
-    #         "money": "41223",
-    #         "method": "使用方法（更新）",
-    #         "num": "2"
-    #     }
-    #     from Utils.EncryptUtils import get_md5_sign
-    #     sign = get_md5_sign(data)
-    #     r = requests.put("http://localhost:5000/v4/coupon?sign=%s" % (sign,), data=data)
-    #     self.assertEqual(r.status_code, 200)
-    #     print r.text
+    def test_update_coupon(self):
+        data = {
+            "game": [{"apk_id": 783}],
+            "users_type": "255,3",
+            "vip_user": "3",
+            "specify_user": "123,324",
+            "id": 1,
+            "title": "测试卡券(更新)",
+            "stime": 123234300,
+            "etime": 123432333,
+            "is_first": 1,
+            "info": "简单描述（更新）",
+            "full": 1231223,
+            "money": 41223,
+            "method": "使用方法（更新）",
+            "num": 2
+        }
+        headers = {"Content-Type": "application/json"}
+        data_json = json.dumps(data)
+        sign = get_cms_md5_sign(data_json)
+        r = requests.put("http://dev.sdkapi.com/msa/v4/coupon?sign=%s" % (sign,), data=data_json, headers=headers)
+        self.assertEqual(r.status_code, 200)
+        print r.text
 
-    # def test_delete_coupon(self):
-    #     data = {
-    #         "id": "1"
-    #     }
-    #     from Utils.EncryptUtils import get_md5_sign
-    #     sign = get_md5_sign(data)
-    #     r = requests.delete("http://localhost:5000/v4/coupon?sign=%s" % (sign,), data=data)
-    #     self.assertEqual(r.status_code, 200)
-    #     print r.text
+    def test_delete_coupon(self):
+        data = {
+            "id": 1
+        }
+        headers = {"Content-Type": "application/json"}
+        data_json = json.dumps(data)
+        sign = get_cms_md5_sign(data_json)
+        r = requests.delete("http://dev.sdkapi.com/msa/v4/coupon?sign=%s" % (sign,), data=data_json, headers=headers)
+        self.assertEqual(r.status_code, 200)
+        print r.text
 
     def test_get_coupon_list(self):
         body_data = {
@@ -82,7 +81,7 @@ class CouponFunctionTest(unittest.TestCase):
             '_token': 'c31fc6b6b2f7b993713c124f8104209d',
             '_appid': 2
         }
-        r = requests.post('http://localhost:5000/msa/v4/coupons', data=body_data)
+        r = requests.post('http://dev.sdkapi.com/msa/v4/coupons', data=body_data)
         print r.text
 
     def tearDown(self):
--- message_service_api/UnitTests/NoticeTest.py
+++ message_service_api/UnitTests/NoticeTest.py
@@ -5,6 +5,8 @@ import unittest
 import requests
 from mongoengine import Q, connect
 
+from Utils.EncryptUtils import get_cms_md5_sign
+
 """
 公告功能测试
 """
@@ -14,87 +16,85 @@ class NoticeFunctionTest(unittest.TestCase):
     def setUp(self):
         connect('users_message', host='localhost', port=27017)
 
-    # def test_add_notice(self):
-    #     from MongoModel.MessageModel import UsersMessage
-    #     from MongoModel.UserMessageModel import UserMessage
-    #     from MongoModel.UserReadMessageLogModel import UserReadMessageLog
-    #     UsersMessage.objects(Q(type='notice') & Q(mysql_id=4)).delete()
-    #     UserMessage.objects(Q(type='notice') & Q(mysql_id=4)).delete()
-    #     UserReadMessageLog.objects(Q(type='notice') & Q(message_id=4)).delete()
-    #     data = {
-    #         "game":"[{\"apk_id\": \"all\"}]",
-    #         "users_type":"255,3",
-    #         "vip_user":"3",
-    #         "specify_user":"",
-    #         "id": "3",
-    #         "title":"测试公告4",
-    #         "content": "公告的详细内容",
-    #         "type":"2",
-    #         "stime":"2134122221",
-    #         "etime":"4523113349",
-    #         "create_time":"2314213324",
-    #         "img":"http://img-url/1.jpg",
-    #         "url":"http://baidu.com",
-    #         "open_type":"1",
-    #         "url_type":"go_finish",
-    #         "button_content":"充值",
-    #         "button_url":"zy://go_charge",
-    #         "button_type": "go_charge",
-    #         "sortby": "1"
-    #     }
-    #     from Utils.EncryptUtils import get_md5_sign
-    #     sign = get_md5_sign(data)
-    #     r = requests.post("http://localhost:5000/msa/v4/notice?sign=%s" % (sign,), data=data)
-    #     self.assertEqual(r.status_code, 200)
-    #     print r.text
+    def test_add_notice(self):
+        data = {
+            "game":[{"apk_id": "all"}],
+            "users_type":"255,3",
+            "vip_user":"3",
+            "specify_user":"",
+            "id": 3,
+            "title":"测试公告4",
+            "content": "公告的详细内容",
+            "type":"2",
+            "stime":"2134122221",
+            "etime":"4523113349",
+            "create_time":"2314213324",
+            "img":"http://img-url/1.jpg",
+            "url":"http://baidu.com",
+            "open_type":"1",
+            "url_type":"go_finish",
+            "button_content":"充值",
+            "button_url":"zy://go_charge",
+            "button_type": "go_charge",
+            "sortby": "1"
+        }
+        headers = {"Content-Type": "application/json"}
+        data_json = json.dumps(data)
+        sign = get_cms_md5_sign(data_json)
+        r = requests.post("http://localhost:5000/msa/v4/notice?sign=%s" % (sign,), data=data_json, headers=headers)
+        self.assertEqual(r.status_code, 200)
+        print r.text
 
-    # def test_update_notice(self):
-    #     data = {
-    #         "game": "[{\"apk_id\": \"all\"}]",
-    #         "users_type": "255,3",
-    #         "vip_user": "3",
-    #         "specify_user": "123,253",
-    #         "id": "4",
-    #         "title": "测试公告",
-    #         "content": "公告的详细内容",
-    #         "type": "2",
-    #         "stime": "12324110",
-    #         "etime": "12434211",
-    #         "create_time": "2314213324",
-    #         "img": "http://img-url/1.jpg",
-    #         "url": "http://haocong.com",
-    #         "open_type": "1",
-    #         "url_type": "go_finish",
-    #         "button_content": "充值",
-    #         "button_url": "zy://go_charge",
-    #         "button_type": "go_charge",
-    #         "sortby": "1"
-    #     }
-    #     from Utils.EncryptUtils import get_md5_sign
-    #     sign = get_md5_sign(data)
-    #     r = requests.put("http://localhost:5000/v4/notice?sign=%s" % (sign,), data=data)
-    #     self.assertEqual(r.status_code, 200)
-    #     print r.text
+    def test_update_notice(self):
+        data = {
+            "game": [{"apk_id": "all"}],
+            "users_type": "255,3",
+            "vip_user": "3",
+            "specify_user": "123,253",
+            "id": "4",
+            "title": "测试公告",
+            "content": "公告的详细内容",
+            "type": "2",
+            "stime": "12324110",
+            "etime": "12434211",
+            "create_time": "2314213324",
+            "img": "http://img-url/1.jpg",
+            "url": "http://haocong.com",
+            "open_type": "1",
+            "url_type": "go_finish",
+            "button_content": "充值",
+            "button_url": "zy://go_charge",
+            "button_type": "go_charge",
+            "sortby": "1"
+        }
+        headers = {"Content-Type": "application/json"}
+        data_json = json.dumps(data)
+        sign = get_cms_md5_sign(data_json)
+        r = requests.put("http://localhost:5000/v4/notice?sign=%s" % (sign,), data=data_json, headers=headers)
+        self.assertEqual(r.status_code, 200)
+        print r.text
 
-    # def test_set_notice_close(self):
-    #     data = {
-    #         "id": "4"
-    #     }
-    #     from Utils.EncryptUtils import get_md5_sign
-    #     sign = get_md5_sign(data)
-    #     r = requests.post("http://localhost:5000/v4/notice/close?sign=%s" % (sign,), data=data)
-    #     self.assertEqual(r.status_code, 200)
-    #     print r.text
+    def test_set_notice_close(self):
+        data = {
+            "id": 4
+        }
+        headers = {"Content-Type": "application/json"}
+        data_json = json.dumps(data)
+        sign = get_cms_md5_sign(data_json)
+        r = requests.post("http://localhost:5000/v4/notice/close?sign=%s" % (sign,), data=data_json, headers=headers)
+        self.assertEqual(r.status_code, 200)
+        print r.text
 
-    # def test_set_notice_open(self):
-    #     data = {
-    #         "id": "4"
-    #     }
-    #     from Utils.EncryptUtils import get_md5_sign
-    #     sign = get_md5_sign(data)
-    #     r = requests.post("http://localhost:5000/v4/notice/open?sign=%s" % (sign,), data=data)
-    #     self.assertEqual(r.status_code, 200)
-    #     print r.text
+    def test_set_notice_open(self):
+        data = {
+            "id": 4
+        }
+        headers = {"Content-Type": "application/json"}
+        data_json = json.dumps(data)
+        sign = get_cms_md5_sign(data_json)
+        r = requests.post("http://localhost:5000/v4/notice/open?sign=%s" % (sign,), data=data_json, headers=headers)
+        self.assertEqual(r.status_code, 200)
+        print r.text
 
     def test_get_notice_list(self):
         body_data = {
