5fdbfea70ac6921e03bb7a0015f26ca9eae77db0
lixingxing <lixingxing@anfan.com>
2017-04-01 09:04:10 +0800

    [pub] update

--- appapi1/app/Model/MongoDB/Model.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Model\MongoDB;
-
-use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
-
-class Model extends Eloquent
-{
-	protected $connection = 'mongodb';
-}
--- appapi1/app/Model/MongoDB/UserMessage.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Model\MongoDB;
-
-class UserMessage extends Model
-{
-    protected $collection = 'sdk_api_user_message';
-}
