202f8cdbfd85b6e34df9aff0b562ebdd0c4b4841
menglingliang <mll@anfan.com>
2017-04-24 18:18:23 +0800

    debug

--- appapi/app/Jobs/OrderNotify.php
+++ appapi/app/Jobs/OrderNotify.php
@@ -26,8 +26,8 @@ class OrderNotify extends Job
 
         $appkey = $procedures->psingKey;
 
-        $data['openid'] = $order->user_sub_id;
-       // $data['ucid'] = $order->cp_uid ? $order->cp_uid : $order->ucid; // todo: 兼容旧系统
+        $data['openid'] = $order->cp_uid ? $order->cp_uid : $order->ucid;
+        $data['ucid'] = $order->cp_uid ? $order->cp_uid : $order->ucid; // todo: 兼容旧系统
         $data['body'] = $order->body;
         $data['subject'] = $order->subject;
         $data['fee'] = sprintf('%.2f',$order->fee);
