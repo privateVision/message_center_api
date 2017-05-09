2e751652254e5795c02d1a87343cb1e5078d5acc
yangchujie <ycj@anfan.com>
2017-05-09 09:00:01 +0800

    fix

--- message_service_api/UnitTests/GiftTest.py
+++ message_service_api/UnitTests/GiftTest.py
@@ -11,19 +11,25 @@ from Utils.EncryptUtils import get_cms_md5_sign
 
 class GiftTest(unittest.TestCase):
 
-    # def test_get_gifts(self):
-    #     body_data = {
-    #         '_sign': 'b87430233acff183fdec47c8fb519add',
-    #         'platform_id': 2,
-    #         '_appid': 778,
-    #         'page': 1,
-    #         'count': 5,
-    #         '_token': '4v42e1pwh5escckokkswskocs'
-    #     }
-    #     # sdkv4test.qcwanwan.com
-    #     r = requests.post('http://sdkv4test.qcwanwan.com/msa/v4/gifts', data=body_data)
-    #     print r.text
-    #     self.assertEqual(r.status_code, 200)
+    def test_get_gifts(self):
+        body_data = {
+            '_appid': 6,
+            '_device_id': '',
+            '_imei': '',
+            '_rid': 1,
+            '_sign_type': 'md5',
+            '_timestamp': '1494288777',
+            '_token': '',
+            '_type': 'json',
+            'page': 1,
+            'pagesize': 100,
+            'platform_id': 4,
+            '_sign': 'e236e1ce57dbe0f399447ec8728b3ceb'
+        }
+        # sdkv4test.qcwanwan.com
+        r = requests.post('http://sdkv4.qcwan.com/msa/v4/gifts', data=body_data)
+        print r.text
+        self.assertEqual(r.status_code, 200)
 
 
     # def test_lingqu_gifts(self):
@@ -40,16 +46,16 @@ class GiftTest(unittest.TestCase):
     #     print r.text
 
 
-    def test_recom_games(self):
-        body_data = {
-            '_sign': 'd5fd8fba3fa2c48a6aacb36b926835a7',
-            'page': 1,
-            'count': 10,
-            '_appid': 846,
-            '_token': 'fk40j37zq1w0woggs80c880w'
-        }
-        r = requests.post('http://sdkv4test.qcwanwan.com/msa/v4/recommend_game_list', data=body_data)
-        print r.text
+    # def test_recom_games(self):
+    #     body_data = {
+    #         '_sign': 'd5fd8fba3fa2c48a6aacb36b926835a7',
+    #         'page': 1,
+    #         'count': 10,
+    #         '_appid': 846,
+    #         '_token': 'fk40j37zq1w0woggs80c880w'
+    #     }
+    #     r = requests.post('http://sdkv4test.qcwanwan.com/msa/v4/recommend_game_list', data=body_data)
+    #     print r.text
 
 
 if __name__ == '__main__':
