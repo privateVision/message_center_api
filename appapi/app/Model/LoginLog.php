43f856c20950b287c3a27ffb4933a6c20bf8255e
lixingxing <lixingxing@anfan.com>
2017-05-17 16:22:36 +0800

    4.1

--- appapi/app/Model/LoginLog.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Model;
-
-class LoginLog extends Model
-{
-    protected $table = 'login_log';
-    protected $primaryKey = 'id';
-}
--- appapi/app/Model/UcuserOauth.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Model;
-
-class UcuserOauth extends Model
-{
-    protected $table = 'ucuser_oauth';
-    const CREATED_AT = 'created_at';
-    const UPDATED_AT = 'updated_at';
-}
