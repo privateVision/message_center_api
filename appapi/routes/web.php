70822347df9bff0d25902760ee6af407169edba5
renyiwondouful@163.com <renyiwondouful@163.com>
2017-05-23 19:45:18 +0800

    api测试平台

--- appapi/routes/web.php
+++ appapi/routes/web.php
@@ -1,5 +1,9 @@
 <?php
-Route::get('/a', function() {
-    return http_build_query(['a' => false, 'b' => true, 'c' => null]);
-    //return redirect('http://www.baidu.com/', 200);
-});
+//Route::get('/a', function() {
+//    return http_build_query(['a' => false, 'b' => true, 'c' => null]);
+//    //return redirect('http://www.baidu.com/', 200);
+//});
+
+Route::get('test','Web\\TestController@indexAction');
+Route::any('login','Web\\TestController@loginAction');
+Route::any('sign','Web\\TestController@signAction');
