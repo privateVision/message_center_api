41b7b5aeacdd747ae37f534ca112d5af9ce5a012
lixingxing <lixingxing@anfan.com>
2017-05-27 09:28:21 +0800

    update

--- appapi/app/Parameter.php
+++ appapi/app/Parameter.php
@@ -34,7 +34,7 @@ class Parameter
             if(preg_match($type_fun_regex, $data)) {
                 return $data;
             } else {
-                throw new Exception (trans('messages.param_format_error', ['key' => $key]), 0); // LANG:argument_format_error
+                throw new Exception (trans('messages.param_format_error', ['key' => $key]), 0);
             }
         }
 
@@ -44,7 +44,7 @@ class Parameter
     public function tough($key, $type_fun_regex = null) {
         $data = @$this->_data[$key];
         if($data === null || $data === '') {
-            throw new Exception (trans('messages.param_missing', ['key' => $key]), 0); // LANG:argument_missing
+            throw new Exception (trans('messages.param_missing', ['key' => $key]), 0);
         }
 
         if(is_string($type_fun_regex) && method_exists($this, $type_fun_regex)) {
@@ -59,7 +59,7 @@ class Parameter
             if(preg_match($type_fun_regex, $data)) {
                 return $data;
             } else {
-                throw new Exception (trans('messages.param_format_error', ['key' => $key]), 0); // LANG:argument_format_error
+                throw new Exception (trans('messages.param_format_error', ['key' => $key]), 0);
             }
         }
 
@@ -70,7 +70,7 @@ class Parameter
         $mobile = trim($mobile, '　 ');
 
         if(!preg_match('/^1\d{10}$/', $mobile)) {
-            throw new Exception (trans('messages.mobile_format_error', ['mobile' => $mobile]), 0); // LANG:mobile_format_error
+            throw new Exception (trans('messages.mobile_format_error', ['mobile' => $mobile]), 0);
         }
 
         return $mobile;
@@ -80,15 +80,15 @@ class Parameter
         $username = trim($username, '　 ');
 
         if(preg_match('/^\d+$/', $username)) {
-            throw new Exception (trans('messages.useranme_format_error_d'), 0); // LANG:username_not_d
+            throw new Exception (trans('messages.useranme_format_error_d'), 0);
         }
 
         if(strlen($username) < 6 || strlen($username) > 15) {
-            throw new Exception (trans('messages.useranme_format_error_l'), 0); // LANG:username_length
+            throw new Exception (trans('messages.useranme_format_error_l'), 0);
         }
 
         if(preg_match('/[^a-zA-Z0-9]+/', $username)) {
-            throw new Exception (trans('messages.useranme_format_error_dw'), 0); // LANG:username_dw
+            throw new Exception (trans('messages.useranme_format_error_dw'), 0);
         }
 
         return $username;
@@ -98,7 +98,7 @@ class Parameter
         $smscode = trim($smscode, '　 ');
 
         if(strlen($smscode) != 6) {
-            throw new Exception (trans('messages.invalid_smscode'), 0); // LANG:smscode_error
+            throw new Exception (trans('messages.invalid_smscode'), 0);
         }
 
         return $smscode;
@@ -108,7 +108,7 @@ class Parameter
         $url = trim($url, '　 ');
 
         if(!preg_match('/^https*:\/\/.*$/', $url)) {
-            throw new Exception (trans('messages.url_format_error', ['url' => $url]), 0); // LANG:url_error
+            throw new Exception (trans('messages.url_format_error', ['url' => $url]), 0);
         }
 
         return $url;
@@ -116,7 +116,7 @@ class Parameter
 
     protected function password($password) {
         $password = trim($password, '　 ');
-        if($password == "") throw new Exception (trans('messages.password_empty'), 0); // LANG:password_not_empty
+        if($password == "") throw new Exception (trans('messages.password_empty'), 0);
 
         return $password;
     }
@@ -130,7 +130,7 @@ class Parameter
         $len = $len1 + ($len2 - $len1) / 2;
 
         if($len > 14) {
-            throw new Exception (trans('messages.nickname_format_error_l'), 0); // LANG:nickname_format_error
+            throw new Exception (trans('messages.nickname_format_error_l'), 0);
         }
 
         return $nickname;
@@ -145,7 +145,7 @@ class Parameter
         $len = $len1 + ($len2 - $len1) / 2;
 
         if($len > 10) {
-            throw new Exception (trans('messages.subnickname_format_error_l'), 0); // LANG:nickname_format_error
+            throw new Exception (trans('messages.subnickname_format_error_l'), 0);
         }
 
         return $nickname;
