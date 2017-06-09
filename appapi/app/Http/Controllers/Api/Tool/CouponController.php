325359ceeaa720343207020959b0d9dad0876f41
余卫军 <576983530@qq.com>
2017-06-09 11:19:12 +0800

    s

--- appapi/app/Http/Controllers/Api/Tool/CouponController.php
+++ appapi/app/Http/Controllers/Api/Tool/CouponController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\Api\Tool;
 
+use App\Model\ZyCouponLog;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class CouponController extends Controller
 {
@@ -11,13 +11,8 @@ class CouponController extends Controller
     {
         $couponId = $this->parameter->tough('coupon_id');
 
-        $usedNum = DB::table('zy_coupon_log')
-                    ->where('is_used', '=', 1)
-                    ->where('coupon_id', '=', $couponId)
-                    ->count();
-        $total = DB::table('zy_coupon_log')
-                    ->where('coupon_id', '=', $couponId)
-                    ->count();
+        $usedNum = ZyCouponLog::where('is_used', '=', 1)->where('coupon_id', '=', $couponId)->count();
+        $total = ZyCouponLog::where('coupon_id', '=', $couponId)->count();
         return [
             'used_num'=>$usedNum,
             'total' => $total
