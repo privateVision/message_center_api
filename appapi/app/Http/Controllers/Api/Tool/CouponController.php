d6239a83612a90444fb0447d3ac3703f93a1237c
lixingxing <lixingxing@anfan.com>
2017-06-09 14:03:32 +0800

    update

---
+++ appapi/app/Http/Controllers/Api/Tool/CouponController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\Tool;
+
+use App\Model\ZyCouponLog;
+
+class CouponController extends Controller
+{
+    public function GetCouponUsedAction()
+    {
+        $couponId = $this->parameter->tough('coupon_id');
+
+        $usedNum = ZyCouponLog::where('is_used', '=', 1)->where('coupon_id', '=', $couponId)->count();
+        $total = ZyCouponLog::where('coupon_id', '=', $couponId)->count();
+        return [
+            'used_num'=>$usedNum,
+            'total' => $total
+        ];
+    }
+}
